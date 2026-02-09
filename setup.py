f04eb07b86fb8ddddf09f99c70d5793cd34323f5
ci_volc_sdk_gen <ci_volc_sdk_gen@bytedance.com>
2026-02-09 14:13:57 +0800

    feat: auto generate for redis-Python-2020-12-07-online-2262-2026_02_09_14_13_44

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "byteplus-python-sdk-v2"
-VERSION = "3.0.31"
+VERSION = "3.0.32"
 # To install the library, run the following
 #
 # python setup.py install
